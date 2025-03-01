f12049b0a214ceb8f0a18760df7e61116c7d4c51
bel-mont-dev <bel-mont-dev@proton.me>
2025-03-01 20:30:12 +0900

    bitwise equals

--- Part1-Ch2/bit_logical_operators.c
+++ Part1-Ch2/bit_logical_operators.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int equals(int x, int y)
+{
+  // Using bit operators and logical operators, return 1 when x and y are true
+  // return 0 otherwise
+  const int result = !(x ^ y);
+  printf("x & y %08x\n", result);
+  return result;
+}
+
 int main() {
   const int a = 0x55;
   const int b = 0x46;
@@ -13,6 +22,17 @@ int main() {
   printf("a || b %08x\n", a || b);
   printf("!a || !b %08x\n", !a || !b);
   printf("a && ~b %08x\n", a && ~b);
+
+  printf("Equals 50 == 40: %d\n", equals(50, 40));
+  printf("Equals 45 == 45: %d\n", equals(45, 45));
+  printf("Equals 2 == 1: %d\n", equals(2, 1));
+  printf("Equals 2 == 2: %d\n", equals(2, 2));
+  printf("Equals 24123 == 7182: %d\n", equals(24123, 7182));
+  printf("Equals 24123 == 24123: %d\n", equals(24123, 24123));
+
+  const int x = 0x7;
+  const int y = 0x2;
+  printf("7 ^ 2: %08x\n", (x & ~y) | (~x & y));
 
   return 0;
 }
--- Part1-Ch2/vax_bytes.c
+++ Part1-Ch2/vax_bytes.c
@@ -11,5 +11,13 @@ int bool_or(int x, int y) {
 /* 2.13 Compute x^y using only calls to functions bis and bic */
 int bool_xor(int x, int y) {
     int result = bis(bic(x, y), bic(y, x));
+
+    // x ^ y = (x & ~y) | (~x & y)
+    // Example with x = 0x7 (0111), y = 0x2 (0010)
+    // (0111 & ~0010) | (~0111 & 0010)
+    // (0111 & 1101) | (1000 & 0010)
+    // 0101 | 0000
+    // 0101
+    // 0x5
     return result;
 }
